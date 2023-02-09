0e18474dec574799ae6af613551af1c4c14385bf
Metythorn Penn <64040104+MetythornPenn@users.noreply.github.com>
2023-02-09 10:39:12 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function(req, res){
+    //console.log(req.body.cityName);
+    const query = req.body.cityName;
+    const apiKey = "861ff9550846203a24c59504d4523482"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid="+ apiKey +"&units=" + unit
+    
+    https.get(url, function(respone) {
+        console.log(respone.statusCode);
+
+        respone.on('data', function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+            console.log(temp)
+            //console.log(weatherData)
+            res.write('<p>The weather is currently ' + weatherDescription  + '</p>')
+            res.write('<h1>The temperature in '+ query +' is ' + temp + ' degrees Celcius</h1>')
+            res.write('<img src=' + imageURL + '>');
+            res.send()
+            
+        });
+
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.listen(3000, function() {
+        console.log('Server is running on port 3000');
+    })
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
